fdc1fd09d22362307b225340289f2eb70a5eff13
kurarin <fuji.ktbl@gmail.com>
2020-03-11 18:07:43 +0900

    start.cmdの改変

--- start.cmd
+++ start.cmd
@@ -12,12 +12,15 @@ if exist bin\php\php.exe (
 if exist PocketMine-MP.phar (
 	set POCKETMINE_FILE=PocketMine-MP.phar
 ) else (
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	) else (
 	echo PocketMine-MP.phar not found
 	echo Downloads can be found at https://github.com/pmmp/PocketMine-MP/releases
 	pause
 	exit 1
 )
-
+)
 if exist bin\mintty.exe (
 	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 ) else (
